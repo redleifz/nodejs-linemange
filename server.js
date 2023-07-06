21a2f03885825a22106e8c2bbeb7862efaf7b1ed
Jongjate Choomjairug <jongjate@isoftel.co.th>
2023-07-06 09:38:19 +0700

    fix

--- server.js
+++ server.js
@@ -23,62 +23,50 @@ app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 })
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.log('Connected to MySQL Server!');
-})
-
-
-
 
 app.post('/api/linesend', (req, res) => {
     const roomId = req.body.roomId;
     const roomPassword = req.body.roomPassword;
     const roomContent = req.body.roomContent;
-
-    connection.query('SELECT line_token FROM tb_line_manage WHERE room_Id = ? AND room_password = ?', [roomId, roomPassword], (err, result) => {
-        if (err) throw err;
-        if (result.length > 0) {
-
-            const line_token = result[0].line_token;
-
-            async function sendLineNotification(roomContent, line_token) {
-                try {
-                    await axios.post(
-                        'https://notify-api.line.me/api/notify',
-                        {
-                            message: roomContent,
-                        },
-                        {
-                            headers: {
-                                'Content-Type': 'application/x-www-form-urlencoded',
-                                Authorization: `Bearer ${line_token}`,
-                            },
-                        }
-                    );
-                    res.status(200).json({ "status": "Success", "content": roomContent });
-                } catch (error) {
-                    if (error.response && error.response.data) {
-                        // console.error('Error sending Line notification:', error.response.data);
-                        res.status(400).send(`Error sending Line notification: ${error.response.data.message}`);
-                    } else {
-                        // console.error('Error sending Line notification:', error);
-                        res.status(500).send('Internal Server Error');
-                    }
-                }
-            }
-
-
-            // Usage
-            sendLineNotification(roomContent, line_token);
-
-
-        } else {
-            res.status(400).json({ "status": "fail auth", "message": 'RoomID and Password Invalid.' });
+  
+    connection.query('SELECT line_token FROM tb_line_manage WHERE room_Id = ? AND room_password = ?', [roomId, roomPassword], async (err, result) => {
+      if (err) {
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+      if (result.length > 0) {
+        const lineToken = result[0].line_token;
+        
+        try {
+          await sendLineNotification(roomContent, lineToken);
+          res.status(200).json({ "status": "Success", "content": roomContent });
+        } catch (error) {
+          if (error.response && error.response.data) {
+            res.status(400).send(`Error sending Line notification: ${error.response.data.message}`);
+          } else {
+            res.status(500).send('Internal Server Error');
+          }
+        } finally {
+          connection.end(); // Close the MySQL connection
         }
-    })
-
-})
+      } else {
+        res.status(400).json({ "status": "fail auth", "message": 'RoomID and Password Invalid.' });
+      }
+    });
+  });
+  
+  async function sendLineNotification(roomContent, lineToken) {
+    await axios.post(
+      'https://notify-api.line.me/api/notify',
+      { message: roomContent },
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Authorization: `Bearer ${lineToken}`,
+        },
+      }
+    );
+  }
 
 
 
